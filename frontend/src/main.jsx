5d0051fab468add48b6d895c7b1f4be826720439
ikeshav26 <keshavgilhotra4@gmail.com>
2025-07-16 16:41:12 +0530

    ✨ Vercel analytics added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
  
     <BrowserRouter>
+    <Analytics/>
     <AppProvider>
        <App />
     </AppProvider>
