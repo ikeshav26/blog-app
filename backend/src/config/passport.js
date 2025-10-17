88f12358cfd90eb06f98458d1d295d07bfb65420
ikeshav26 <keshavgilhotra4@gmail.com>
2025-10-18 01:36:55 +0530

    ✨ Fix

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `/auth/google/callback`,
+      callbackURL: `${process.env.API_URL}/auth/google/callback`,
       scope: ['profile', 'email'], // Explicitly request email and profile
     },
     async (accessToken, refreshToken, profile, done) => {
